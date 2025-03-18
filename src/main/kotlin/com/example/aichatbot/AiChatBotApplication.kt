37a98313040f33320c201eed6349ca7409a66087
최호빈 <hobinchoi0116@gmail.com>
2025-03-18 12:10:12 +0900

    setting: 프로젝트 초기화

---
+++ src/main/kotlin/com/example/aichatbot/AiChatBotApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aichatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiChatBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/aichatbot/AiChatBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.aichatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
